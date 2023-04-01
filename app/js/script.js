fa2ba0eba75c84d148d81a8f6a0cf54dabf17821
iggyyes <igamwisniewska@gmail.com>
2023-04-01 21:19:38 +0100

    add js local storage

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,89 @@
 //doing the list first
 const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+//tasks list
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const tasksContainer = document.querySelector('[data-tasks]')
 
-let lists = ['pieczareczki', 'jakies gowno']
+
+
+//local storage
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY) //to jest od klikonio i brania jaki to jest ciul 
+
+//selects a clicked list and gives it the class of active
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
+
+//delete button for active list
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
+
+// adds a new list to the other visible lists
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    saveAndRender()
+})
+
+function createList(name) {
+   return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
 
 function render() {
     clearElement(listsContainer)
+    renderLists()
+
+    //removes active list and its tasks list
+    const selectedList = lists.find(list => list.id === selectedListId)
+    if (selectedListId == null) {
+        listDisplayContainer.style.display = 'none'
+    } else {
+        //changes tasks title to the active list name
+        listDisplayContainer.style.display = ''
+        listTitleElement.innerText = selectedList.name
+    }
+}
+
+//for adding new list items to actual list 
+function renderLists() {
     // <li class="list-name">work</li>
     lists.forEach(list => {
         const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
         listElement.classList.add('list-name')
-        listElement.innerText = list
+        listElement.innerText = list.name
+        //adds active class to selected list
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
